a221fa56c63bb647a204ce3eae8bbc238afa89cf
amitsavani-45 <apsavani4518@gmail.com>
2026-02-13 23:13:50 +0530

    git diff

---
+++ diff.js
@@ -0,0 +1,11 @@
+/*git diff
+
+Definition:
+git diff shows the differences between files or changes that have not yet been committed.
+
+Purpose:
+
+Compare file versions
+
+Review modifications before commit
+*/
