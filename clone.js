69a398c9d3f8c450ec0faa438e96fd0b39d09076
amitsavani-45 <apsavani4518@gmail.com>
2026-02-13 23:12:07 +0530

    git clone

---
+++ clone.js
@@ -0,0 +1,10 @@
+/*git clone
+
+Definition:
+git clone is used to create a copy of an existing remote repository on a local system. It downloads all project files and version history.
+
+Purpose:
+
+Copy project from remote server
+
+Start working on existing project*/
