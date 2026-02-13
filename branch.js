84b5684069141812d3539f565325055c2c81deb5
amitsavani-45 <apsavani4518@gmail.com>
2026-02-13 23:14:50 +0530

    git branch

---
+++ branch.js
@@ -0,0 +1,11 @@
+/*git branch
+
+Definition:
+git branch is used to create, list, or delete branches in a repository. Branches allow parallel development without affecting the main code.
+
+Purpose:
+
+Work on new features separately
+
+Manage multiple versions
+*/
