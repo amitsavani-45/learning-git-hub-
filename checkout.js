f28d59b56042a0e04b88517d709ded1447a3afd6
amitsavani-45 <apsavani4518@gmail.com>
2026-02-15 21:51:20 +0530

    checkout

---
+++ checkout.js
@@ -0,0 +1,18 @@
+/*git checkout is a Git command used to change the current state of the working directory and move between different branches or commits in a repository.
+
+In simple terms, it allows a developer to switch from one version of the project to another.
+
+ Definition
+
+git checkout is used to:
+
+Switch between branches
+
+Create and switch to a new branch
+
+View or restore a specific commit
+
+Restore files to their previous state
+
+It changes the position of HEAD (the pointer that refers to the current branch or commit).
+*/
