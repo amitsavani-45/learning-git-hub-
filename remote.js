271e38ec5308a61f954455cb97d1c825d88b45a5
amitsavani-45 <apsavani4518@gmail.com>
2026-02-15 21:56:19 +0530

    remote

---
+++ remote.js
@@ -0,0 +1,31 @@
+/*Definition
+
+git remote is a Git command used to manage connections between your local repository and a remote repository (usually hosted on platforms like GitHub).
+
+A remote repository is a version of your project stored on the internet or a server that allows collaboration and backup.
+
+ Purpose of git remote
+
+The main purpose of git remote is to:
+
+Connect a local repository to a remote repository
+
+View remote repository details
+
+Add or remove remote connections
+
+Rename remote repositories
+
+It helps in synchronizing local work with online repositories.
+
+ What is a Remote?
+
+A remote is simply a reference (shortcut name) for a repository hosted elsewhere.
+
+By default, when you clone a repository, Git automatically creates a remote named:
+
+origin
+
+
+origin is just a default name — it represents the URL of the remote repository.
+*/
