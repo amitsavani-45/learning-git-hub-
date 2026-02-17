8011799ad380587f4d4c98807ffe4272bfb962db
amitsavani-45 <apsavani4518@gmail.com>
2026-02-17 22:43:54 +0530

    merge

---
+++ merge.js
@@ -0,0 +1,28 @@
+/*What is git merge?
+
+git merge is a Git command used to combine changes from one branch into another branch.
+
+It helps developers integrate work done in different branches (like feature branches) into the main branch (such as main or master).
+
+🔹 Why Do We Use git merge?
+
+In software development:
+
+Different features are developed in separate branches.
+
+After completing the feature, we need to add (integrate) it into the main project.
+
+git merge allows us to combine those changes safely.
+
+🔹 Basic Concept
+
+Suppose you have:
+
+main branch → Production-ready code
+
+feature-login branch → Login feature development
+
+When the login feature is completed, you merge it into main.
+
+👉 Git takes the changes from feature-login and integrates them into main.
+*/
