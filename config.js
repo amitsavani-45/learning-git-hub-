522b3144dd8337c20e10f2562953141c22283ffc
amitsavani-45 <apsavani4518@gmail.com>
2026-02-27 21:05:54 +0530

    config

---
+++ config.js
@@ -0,0 +1,26 @@
+/*git config (Aapki Pehchaan Set Karna)
+Jab bhi aap Git mein koi change save (commit) karte hain, Git uske saath ek "Author" ka naam aur email attach karta hai. Isse team ko pata chalta hai ki kisne kya kaam kiya hai.
+
+Syntax:
+
+Bash
+git config --global user.name "Aapka Naam"
+git config --global user.name "aapkaemail@example.com"
+--global ka matlab: Iska matlab hai ki ye settings aapke poore computer ke liye hain. Aapko har project ke liye ise baar-baar set nahi karna padega.
+
+Check kaise karein? Agar aap dekhna chahte hain ki kya set hua hai, to ye command chalayein:
+git config --list
+
+2. git version (System Check)
+Ye command sirf ye check karne ke liye hota hai ki aapke computer mein Git sahi se install hua hai ya nahi, aur aap kaunsa version use kar rahe hain.
+
+Usage:
+Terminal ya Command Prompt par bas type karein:
+
+Bash
+git version
+Output Example:
+git version 2.40.1
+
+Kyun zaroori hai? Kabhi-kabhi naye features purane Git versions par kaam nahi karte. Is command se aap confirm kar sakte hain ki aapka Git up-to-date hai.
+*/
