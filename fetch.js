3bc3be5c34088b4224e22e1c4061d94c382040d4
amitsavani-45 <apsavani4518@gmail.com>
2026-03-01 21:20:51 +0530

    fetch

---
+++ fetch.js
@@ -0,0 +1,25 @@
+/*git fetch
+
+Kya karta hai:
+👉 Remote repository (GitHub) se latest changes download karta hai
+👉 Lekin tumhari current files ko change nahi karta
+
+Simple language:
+
+“Sirf check karo aur download karo, merge mat karo”
+
+Example:
+
+git fetch
+
+Kab use kare:
+
+Jab dekhna ho ki remote me kya naya aaya
+
+Safe update chahiye without merge
+
+Difference (important):
+
+git fetch → download only
+
+git pull → download + merge*/
