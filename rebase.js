5513b54f7bb63b9710d1250e5a98ba47c35e166c
amitsavani-45 <apsavani4518@gmail.com>
2026-03-01 21:28:08 +0530

    rebase

---
+++ rebase.js
@@ -0,0 +1,18 @@
+/*git rebase
+
+Kya karta hai:
+👉 Branch history ko clean aur straight line me convert karta hai
+
+Simple language:
+
+“History ko seedha aur clean bana deta hai”
+
+Example:
+
+git rebase main
+
+Kab use kare:
+
+Jab clean commit history chahiye
+
+*/
