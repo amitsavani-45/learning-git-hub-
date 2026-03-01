d31f476d3344fa521705898bdddc24253e3e32c0
amitsavani-45 <apsavani4518@gmail.com>
2026-03-01 21:25:30 +0530

    stash

---
+++ stash.js
@@ -0,0 +1,23 @@
+/*git stash
+
+Kya karta hai:
+👉 Tumhare current unsaved changes ko temporary side me rakh deta hai
+
+Simple language:
+
+“Work ko thodi der ke liye pocket me daal do”
+
+Example:
+
+git stash
+
+Wapas lane ke liye:
+
+git stash pop
+
+Kab use kare:
+
+Jab branch switch karni ho
+
+Jab code incomplete ho but save nahi karna
+*/
