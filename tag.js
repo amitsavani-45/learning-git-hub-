7c40a867f739fb3ee61f172e191e2b6e7bf3fd6e
amitsavani-45 <apsavani4518@gmail.com>
2026-03-01 21:29:23 +0530

    tag

---
+++ tag.js
@@ -0,0 +1,18 @@
+/*git tag
+
+Kya karta hai:
+👉 Kisi commit ko version name deta hai (jaise v1.0)
+
+Simple language:
+
+“Is commit ko special naam de do”
+
+Example:
+
+git tag v1.0
+
+Kab use kare:
+
+Release banate time
+
+Version marking*/
